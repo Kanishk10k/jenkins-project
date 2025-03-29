45133a7ef874fb5f6313b2f6529b7f047aa966a1
Kanishk10k <115786716+Kanishk10k@users.noreply.github.com>
2025-03-29 12:31:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID kanishk10k/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID kanishk10k/$JOB_NAME:latest
 
-docker push vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker push kanishk10k/$JOB_NAME:$BUILD_ID
 
-docker push vaibhavjoshi26/$JOB_NAME:latest
+docker push kanishk10k/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID kanishk10k/$JOB_NAME:$BUILD_ID kanishk10k/$JOB_NAME:latest
